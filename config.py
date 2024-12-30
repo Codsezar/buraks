e48deccbc3cb1da5822fff60301c9123296f423e
Codsezar <codsezar404@gmail.com>
2024-12-30 02:30:30 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,20 +6,20 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "6435225"))
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-BOT_PRIVACY = getenv("BOT_PRIVACY", "https://telegra.ph/Privacy-Policy-for-AnieXEricaMusic-10-06")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", "22724579"))
+API_HASH = getenv("API_HASH", "c80549534e8297a1d4df2abe4a0d2497")
+BOT_PRIVACY = getenv("BOT_PRIVACY", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "7721681605:AAGhVqWWFF920c0WVvV8qrG4NNLJAVrRBus")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Sezar:<password>@cluster0.waszrgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",-1001971806089))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",-1002360781908))
 
-OWNER_ID = int(getenv("OWNER_ID", 7045191057))
+OWNER_ID = int(getenv("OWNER_ID", 7447500710))
 
-OWNER = int(getenv("OWNER", 2105971379))
+OWNER = int(getenv("OWNER", 7447500710))
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
@@ -33,8 +33,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SuperBanSBots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AM_YTSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/atesbocegibioo")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DumanMuzikGr")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "7f92897a59464ddbbf00f06cd6bda7fc")
@@ -42,7 +42,7 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 5242880000))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 5242880000))
 
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION", "BAFav-MAg5PKno7zreuMMssM68Y4LBkloOO-NNCuWFnHzCgYtA-VzbbRsoIiUtgiSiNG9rPw864-n7xHrYqGbjhrbpYEp6xqe9adqsYVtcd0r70VJpxF6yJWCa7TMdnT7GP_Ovu9Trh29PERPCa4RRgRhSHR5HlDD7MVI2rcPR4s4zpyf_WKwXzulBHWjpeKAUsdMHD7sMpXOFC86OyAqUeTVwOsJDzPVGyLvFHsqHM8db46dBnhaesv_fI2haoN3VuD6-ycKQsypMZucuAeq1zXeevIJXK0m4nLV9kNfv4ZxQOECVunoY3ccwj2F1u8L8grXMQlDXgEOcNb2tGGL8AwZa67ngAAAAHGDh3iAA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -57,11 +57,11 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = getenv("STATS_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+START_IMG_URL = getenv("START_IMG_URL", "https://t.me/Dumancchannel/9")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://t.me/Dumancchannel/9")
+PLAYLIST_IMG_URL = "https://t.me/Dumancchannel/9"
+STATS_IMG_URL = getenv("STATS_IMG_URL", "https://t.me/Dumancchannel/9")
+TELEGRAM_AUDIO_URL = "https://t.me/Dumancchannel/9"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
